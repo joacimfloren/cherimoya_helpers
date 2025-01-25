3337e4cb00b871e31c8b33a2620d98521bc4eea4
LAPTOP-BSOT30GV\joaci <joacimfloren@gmail.com>
2025-01-25 20:43:08 +0100

    Added README.md and renamed

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 #     deps = [l.strip() for l in f.readlines() if l.strip() != "" and not l.strip().startswith("#")]
 
 setup(
-    name='cherimoya-helpers',
+    name='cherimoya_helpers',
     version='1.0.12',
     description='Useful python helpers for lambda',
-    url='https://github.com/joacimfloren/cherimoya-helpers.git',
+    url='https://github.com/joacimfloren/cherimoya_helpers.git',
     author='Joacim Florén',
     author_email='joacim.floren@gmail.com',
     license='MIT',
